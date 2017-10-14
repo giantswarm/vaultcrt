6b043d55508f4cefbddba1dbd09823675dfbb68b
Tim Schindler <xh3b4sd@gmail.com>
2017-10-14 17:51:35 +0200

    added logger

--- vaultcrt.go
+++ vaultcrt.go
@@ -2,15 +2,18 @@ package vaultcrt
 
 import (
 	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
 	vaultclient "github.com/hashicorp/vault/api"
 )
 
 type Config struct {
+	Logger      micrologger.Logger
 	VaultClient *vaultclient.Client
 }
 
 func DefaultConfig() Config {
 	config := Config{
+		Logger:      nil,
 		VaultClient: nil,
 	}
 
@@ -18,15 +21,20 @@ func DefaultConfig() Config {
 }
 
 type VaultCrt struct {
+	logger      micrologger.Logger
 	vaultClient *vaultclient.Client
 }
 
 func New(config Config) (*VaultCrt, error) {
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty")
+	}
 	if config.VaultClient == nil {
 		return nil, microerror.Maskf(invalidConfigError, "config.VaultClient must not be empty")
 	}
 
 	c := &VaultCrt{
+		logger:      config.Logger,
 		vaultClient: config.VaultClient,
 	}
 
